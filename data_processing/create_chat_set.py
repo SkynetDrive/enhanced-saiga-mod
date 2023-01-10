06bca5ea119396acf346f0c4f650df73e06a9e9c
SkynetDrive <131639668+SkynetDrive@users.noreply.github.com>
2023-01-10 08:22:11 +0000

    English locales update

--- data_processing/create_chat_set.py
+++ data_processing/create_chat_set.py
@@ -1,4 +1,14 @@
 import json
 import re
 import random
-imp
+import sys
+from datasets import load_dataset
+from tqdm import tqdm
+from data_processing.bad_substrings import has_bad_ss
+from datasketch import MinHash, MinHashLSH, LeanMinHash
+from itertools import tee
+
+
+def revert_flattening(records):
+    fixed_records = []
+    for key, va
