359573cedc6eb2e8fdc4eb4fcd7128032e16c1b5
SkynetDrive <131639668+SkynetDrive@users.noreply.github.com>
2023-01-31 07:40:55 +0000

    fixed var name

--- data_processing/create_chat_set.py
+++ data_processing/create_chat_set.py
@@ -95,4 +95,9 @@ def undup_alpaca(alpaca_records, num_perm: int = 32, threshold: float = 0.3, deb
         is_dup = False
         for other_idx in lsh.query(minhash):
             other_record = alpaca_records[other_idx]
-            other_minhash = LeanMinHash.deserial
+            other_minhash = LeanMinHash.deserialize(other_record["minhash"])
+            if minhash.jaccard(other_minhash) > threshold:
+                if debug:
+                    print()
+                    print("=========================")
+                    print(record["messages"][0]["conte
