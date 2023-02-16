21e4dfaa563e3122df4c80d4fcd3ede5b682a4ff
SkynetDrive <131639668+SkynetDrive@users.noreply.github.com>
2023-02-16 06:14:51 +0000

    added styles

--- data_processing/create_chat_set.py
+++ data_processing/create_chat_set.py
@@ -177,4 +177,11 @@ for row in load_dataset("IlyaGusev/ru_turbo_alpaca", split="train"):
 
 random.shuffle(records)
 border = int(0.95 * len(records))
-train_records = records[
+train_records = records[:border]
+val_records = records[border:]
+with open(train_path, "w") as w:
+    for record in train_records:
+        w.write(json.dumps(record, ensure_ascii=False).strip() + "\n")
+with open(val_path, "w") as w:
+    for record in val_records:
+        w.write(js
