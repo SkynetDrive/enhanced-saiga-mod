69a8f11eb481b7f27fd02a164a491f06bbb4bc6c
SkynetDrive <131639668+SkynetDrive@users.noreply.github.com>
2023-02-18 20:03:20 +0000

    Added a comment [skip CI]

--- data_processing/create_instruct_set_v2.py
+++ data_processing/create_instruct_set_v2.py
@@ -8,4 +8,15 @@ from tqdm import tqdm
 from itertools import tee
 from datasketch import MinHash, MinHashLSH, LeanMinHash
 from data_processing.bad_substrings import has_bad_ss
-fr
+from joblib import Parallel, delayed
+
+
+def calc_max_length(records):
+    return max([sum([len(m["content"]) for m in r["messages"]]) for r in records])
+
+
+def re_tokenize(text):
+    return re.findall(r'[а-яё-]+|[a-z-]+|\d+|\S', text, re.I)
+
+
+def ngrams
