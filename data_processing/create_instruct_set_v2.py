57dd1333db6e63743ee20fe18452933be0306159
SkynetDrive <131639668+SkynetDrive@users.noreply.github.com>
2023-03-05 05:36:57 +0000

    Version 0.0.6

--- data_processing/create_instruct_set_v2.py
+++ data_processing/create_instruct_set_v2.py
@@ -121,4 +121,11 @@ def main(train_path, val_path):
     #     if row["system_prompt"]:
     #         message += row["system_prompt"]
     #     if row["system_prompt"] and row["question"]:
+    #         message += "\n"
+    #     if row["question"]:
+    #         message += row["question"]
+    #     output = row["response"]
+    #     if has_bad_ss([{"content": output}]):
+    #         continue
+    #     messages = [
     #    
