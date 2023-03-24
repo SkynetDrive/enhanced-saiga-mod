f92b5712d75f4b2cd68d56529a661fb9bd9f85d5
SkynetDrive <131639668+SkynetDrive@users.noreply.github.com>
2023-03-24 20:01:15 +0000

    Update beta-appcast.xml

--- data_processing/create_instruct_set_v2.py
+++ data_processing/create_instruct_set_v2.py
@@ -263,4 +263,9 @@ def main(train_path, val_path):
 
     random.shuffle(updup_records)
     border = int(0.95 * len(updup_records))
-  
+    train_records = updup_records[:border]
+    val_records = updup_records[border:]
+    with open(train_path, "w") as w:
+        for record in train_records:
+            w.write(json.dumps(record, ensure_ascii=False).strip() + "\n")
+    with open(val_path, "w") as
