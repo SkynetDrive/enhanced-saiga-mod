da8ba0ed73f1fd7d81b9ffe584f9c3c470261422
SkynetDrive <131639668+SkynetDrive@users.noreply.github.com>
2023-03-27 14:05:51 +0000

    compat

--- data_processing/instruct_dataset.py
+++ data_processing/instruct_dataset.py
@@ -1,3 +1,16 @@
 import json
 import random
-from ty
+from typing import List, Dict
+
+import torch
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer
+
+from tqdm import tqdm
+
+
+class InstructDataset(Dataset):
+    def __init__(
+            self,
+            original_records: List[Dict],
+            tokenizer
