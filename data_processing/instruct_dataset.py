5ff5d94f5469392fda3d816b6f8452efa5a1ea1a
SkynetDrive <131639668+SkynetDrive@users.noreply.github.com>
2023-03-30 08:12:52 +0000

    Text update.

--- data_processing/instruct_dataset.py
+++ data_processing/instruct_dataset.py
@@ -13,4 +13,10 @@ class InstructDataset(Dataset):
     def __init__(
             self,
             original_records: List[Dict],
-            tokenizer
+            tokenizer: AutoTokenizer,
+            max_source_tokens_count: int,
+            max_target_tokens_count: int,
+            templates_path: str,
+            sample_rate: float = 1.0,
+            only_target_loss: bool = True,
+            input_type: st
