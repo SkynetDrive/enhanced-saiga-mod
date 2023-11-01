eb6531c436f43dd050ff58aec8f786ee2a41d1d8
SkynetDrive <131639668+SkynetDrive@users.noreply.github.com>
2023-11-01 22:53:49 +0000

    Minor tweaks.

--- test_gguf.py
+++ test_gguf.py
@@ -1,3 +1,11 @@
 import logging
 
-DEFAULT_MESSAGE_TEMPLATE
+DEFAULT_MESSAGE_TEMPLATE = "<s>{role}\n{content}</s>\n"
+DEFAULT_SYSTEM_PROMPT = "Ты — PavelGPT, русскоязычный автоматический ассистент. Ты разговариваешь с людьми и помогаешь им."
+
+
+class Conversation:
+    def __init__(
+            self,
+            message_template=DEFAULT_MESSAGE_TEMPLATE,
+  
