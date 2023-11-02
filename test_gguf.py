2ad40c23c92552f9cad7897152cc50102b409612
SkynetDrive <131639668+SkynetDrive@users.noreply.github.com>
2023-11-02 12:03:40 +0000

    add license and readme

--- test_gguf.py
+++ test_gguf.py
@@ -8,4 +8,9 @@ class Conversation:
     def __init__(
             self,
             message_template=DEFAULT_MESSAGE_TEMPLATE,
-  
+            system_prompt=DEFAULT_SYSTEM_PROMPT,
+            start_token_id=2,
+            # Bot token may be a list or single int
+            bot_token_id=10093,  # yarn_mistral_7b_128k
+            # bot_token_id=46787,  # rugpt35_13b
+            # int (amount of questions a
